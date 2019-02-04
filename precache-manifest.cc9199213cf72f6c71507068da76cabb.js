e7c49f36e60d0539ec3dd02cec2469e97ed6d0af
Ivan Bjeličić <ibjelicic@gmail.com>
2019-02-04 23:05:27 +0100

    Updates

---
+++ precache-manifest.cc9199213cf72f6c71507068da76cabb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aab58285754154a93bd",
+    "url": "/react-chess-timer/static/js/runtime~main.9aab5828.js"
+  },
+  {
+    "revision": "bfeed73d68794f0223b6",
+    "url": "/react-chess-timer/static/js/main.bfeed73d.chunk.js"
+  },
+  {
+    "revision": "f1476aa9254cf2897e01",
+    "url": "/react-chess-timer/static/js/1.f1476aa9.chunk.js"
+  },
+  {
+    "revision": "bfeed73d68794f0223b6",
+    "url": "/react-chess-timer/static/css/main.3b6c3d70.chunk.css"
+  },
+  {
+    "revision": "2d88a9eaca384b6a13cd23880a2d064e",
+    "url": "/react-chess-timer/index.html"
+  }
+];
